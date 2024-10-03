28a02aa6030c679d722d98b073b0b16b0d6729ac
Logan Croley <logan.croley.18@gmail.com>
2024-10-03 16:54:40 -0400

    mvp of rpc

---
+++ counter.gd
@@ -0,0 +1,21 @@
+extends Control
+
+signal game_finished()
+
+var count := 0
+
+@onready var label: Label = $Panel/Label
+@onready var increment: Button = $Panel/increment
+@onready var decrement: Button = $Panel/decrement
+
+@rpc("any_peer","call_local")
+func update_score(amt: int)-> void:
+	count += amt
+	label.set_text(str(count))
+
+func _on_increment_pressed() -> void:
+	update_score.rpc(1)
+
+
+func _on_decrement_pressed() -> void:
+	update_score.rpc(-1)
--- lobby.gd
+++ lobby.gd
@@ -20,24 +20,45 @@ func _ready() -> void:
 
 #region Network callbacks from SceneTree
 func _player_connected(_id: int)-> void: 
-	pass
+	var counter: Control = load("res://counter.tscn").instantiate()
+	# Connect deferred so we can safely erase it from the callback.
+	counter.game_finished.connect(_end_game, CONNECT_DEFERRED)
+	get_tree().get_root().add_child(counter)
+	hide()
 
 func _player_disconnected(_id: int)-> void: 
-	pass
+	if multiplayer.is_server():
+		_end_game("Client disconnected.")
+	else:
+		_end_game("Server disconnected.")
 
 func _connected_ok()-> void:
 	pass
 
 func _connected_fail()-> void:
-	pass
-	
+	_set_status("Couldn't connect.", false)
+
+	multiplayer.set_multiplayer_peer(null)  # Remove peer.
+	host_button.set_disabled(false)
+	join_button.set_disabled(false)
+
 func _server_disconnected()-> void:
-	pass
+	_end_game("Server disconnected.")
 #endregion
 
 #region Game initialization methods
 func _end_game(with_error: String="") -> void:
-	pass
+	if has_node("/root/Pong"):
+		# Erase immediately, otherwise network might show
+		# errors (this is why we connected deferred above).
+		get_node(^"/root/Pong").free()
+		show()
+
+	multiplayer.set_multiplayer_peer(null)  # Remove peer.
+	host_button.set_disabled(false)
+	join_button.set_disabled(false)
+
+	_set_status(with_error, false)
 	
 func _set_status(text: String, is_ok: bool) -> void:
 	if is_ok:
@@ -48,8 +69,32 @@ func _set_status(text: String, is_ok: bool) -> void:
 		status_fail.set_text(text)
 		
 func _on_host_pressed() -> void:
-	pass
-	
+	peer = ENetMultiplayerPeer.new()
+	var err := peer.create_server(port)
+	if err != OK:
+		# Is another server running?
+		_set_status("Can't host, address in use.",false)
+		return
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+
+	multiplayer.set_multiplayer_peer(peer)
+	host_button.set_disabled(true)
+	join_button.set_disabled(true)
+	_set_status("Waiting for player...", true)
+	get_window().title = ProjectSettings.get_setting("application/config/name") + ": Server"
+
+
 func _on_join_pressed() -> void:
-	pass
+	var ip := address.get_text()
+	if not ip.is_valid_ip_address():
+		_set_status("IP address is invalid.", false)
+		return
+
+	peer = ENetMultiplayerPeer.new()
+	peer.create_client(ip, port)
+	peer.get_host().compress(ENetConnection.COMPRESS_RANGE_CODER)
+	multiplayer.set_multiplayer_peer(peer)
+
+	_set_status("Connecting...", true)
+	get_window().title = ProjectSettings.get_setting("application/config/name") + ": Client"
 #endregion
